7535c9e81e892783f517b3bfb289e8a515a443dc
caitlinrudolph <brooke_rudolph@hotmail.com>
2019-10-08 19:12:03 -0500

    modified quicksorts to add sorted list testing

--- src/naiveQuickSort.java
+++ src/naiveQuickSort.java
@@ -12,7 +12,7 @@ public class naiveQuickSort {
     static long MAXVALUE =  2000000000;
     static long MINVALUE = -2000000000;
     static int numberOfTrials = 70;
-    static int MAXINPUTSIZE  = (int) Math.pow(2,15);
+    static int MAXINPUTSIZE  = (int) Math.pow(2,10);
     static int MININPUTSIZE  =  1;
     static double prevTimePerTrial = 0;
     static double doublingRatio = 0;
@@ -31,6 +31,9 @@ public class naiveQuickSort {
         runFullExperiment("NQuick-Exp2.txt");
         System.out.println("Running third full experiment...");
         runFullExperiment("NQuick-Exp3.txt");
+
+        System.out.println("Running third full experiment...");
+        runSortedExperiment("NQuick-Sorted.txt");
 
     }
 
@@ -110,6 +113,74 @@ public class naiveQuickSort {
             // but we will randomly generate the search key for each trial
             System.out.print("    Generating test data...");
             long[] testList = createRandomListOfIntegers(inputSize);
+            System.out.println("...done.");
+            System.out.print("    Running trial batch...");
+
+            /* force garbage collection before each batch of trials run so it is not included in the time */
+            System.gc();
+
+            // instead of timing each individual trial, we will time the entire set of trials (for a given input size)
+            // and divide by the number of trials -- this reduces the impact of the amount of time it takes to call the
+            // stopwatch methods themselves
+            //BatchStopwatch.start(); // comment this line if timing trials individually
+
+            // run the trials
+            for (long trial = 0; trial < numberOfTrials; trial++) {
+                // generate a random key to search in the range of a the min/max numbers in the list
+                long testSearchKey = (long) (0 + Math.random() * (testList[testList.length-1]));
+                /* force garbage collection before each trial run so it is not included in the time */
+                // System.gc();
+
+                TrialStopwatch.start(); // *** uncomment this line if timing trials individually
+                /* run the function we're testing on the trial input */
+                long[] foundIndex = sort(testList, 0, testList.length - 1);
+                //if (verifySorted(foundIndex))
+                //{
+                //    System.out.println("Sort Verified");
+                //}
+                batchElapsedTime = batchElapsedTime + TrialStopwatch.elapsedTime(); // *** uncomment this line if timing trials individually
+            }
+
+            //batchElapsedTime = BatchStopwatch.elapsedTime(); // *** comment this line if timing trials individually
+            double averageTimePerTrialInBatch = (double) batchElapsedTime / (double) numberOfTrials; // calculate the average time per trial in this batch
+
+            if (prevTimePerTrial != 0)
+            {
+                doublingRatio = (double) averageTimePerTrialInBatch / (double) prevTimePerTrial;
+            }
+            prevTimePerTrial = averageTimePerTrialInBatch;
+            /* print data for this size of input */
+            resultsWriter.printf("%12d  %15.2f  %15.2f \n", inputSize, averageTimePerTrialInBatch, doublingRatio); // might as well make the columns look nice
+            resultsWriter.flush();
+            System.out.println(" ....done.");
+        }
+    }
+    static void runSortedExperiment(String resultsFileName){
+        try {
+            resultsFile = new FileWriter(ResultsFolderPath + resultsFileName);
+            resultsWriter = new PrintWriter(resultsFile);
+        } catch(Exception e) {
+            System.out.println("*****!!!!!  Had a problem opening the results file "+ResultsFolderPath+resultsFileName);
+            return; // not very foolproof... but we do expect to be able to create/open the file...
+        }
+
+        ThreadCpuStopWatch BatchStopwatch = new ThreadCpuStopWatch(); // for timing an entire set of trials
+        ThreadCpuStopWatch TrialStopwatch = new ThreadCpuStopWatch(); // for timing an individual trial
+
+        resultsWriter.println("#InputSize    AverageTime    Doubling Ratio"); // # marks a comment in gnuplot data
+        resultsWriter.flush();
+        /* for each size of input we want to test: in this case starting small and doubling the size each time */
+        for(int inputSize=MININPUTSIZE;inputSize<=MAXINPUTSIZE; inputSize*=2) {
+            // progress message...
+            System.out.println("Running test for input size "+inputSize+" ... ");
+
+            /* repeat for desired number of trials (for a specific size of input)... */
+            long batchElapsedTime = 0;
+            // generate a list of randomly spaced integers in ascending sorted order to use as test input
+            // In this case we're generating one list to use for the entire set of trials (of a given input size)
+            // but we will randomly generate the search key for each trial
+            System.out.print("    Generating test data...");
+            long[] testList = createSortedListOfIntegers(inputSize);
             System.out.println("...done.");
             System.out.print("    Running trial batch...");
 
